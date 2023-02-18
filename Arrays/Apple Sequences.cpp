dd7405b7d574844f348317e7fc966895d020ca4c
Soumyabethi123 <89738783+Soumyabethi123@users.noreply.github.com>
2023-02-18 19:09:47 +0530

    Create Apple Sequences.cpp

---
+++ Arrays/Apple Sequences.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int appleSequences(int n, int m, string arr){
+        // code here 
+        
+        int i=0,j=0;
+        int ans=0;
+        int count=0;
+        
+        while(j<n){
+            
+            if(arr[j]=='O'){
+                
+                count++;
+            }
+            
+            if(count <= m){
+                
+                ans=max(ans,(j-i)+1);
+            }
+            else{
+                
+                if(arr[i]=='O'){
+                    
+                    count--;
+                    i++;
+                }
+                else{
+                    
+                    i++;
+                }
+                
+                if(count <= m){
+                    
+                    ans=max(ans,(j-i)+1);
+                }
+            }
+            
+            j++;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, M;
+        cin >> N >> M;
+        string arr;
+        cin >> arr;
+        
+        Solution ob;
+        cout << ob.appleSequences(N, M, arr) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
