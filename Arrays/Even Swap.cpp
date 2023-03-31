baffcdd50824616b17945e177227b1f362fd77b6
Soumyabethi123 <89738783+Soumyabethi123@users.noreply.github.com>
2023-03-31 10:03:38 +0530

    Create Even Swap.cpp

---
+++ Arrays/Even Swap.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+        vector <int> lexicographicallyLargest(vector <int> &a,int n)
+        {
+            // Code here
+            int i=1;
+            int start=0;
+            
+            while(i<n){
+                
+                if( (a[i-1]+a[i])%2==0 ){
+                    
+                    i++;
+                }
+                else{
+                    
+                    sort(a.begin()+start,a.begin()+i);
+                    
+                    reverse(a.begin()+start, a.begin()+i);
+                    
+                    start=i;
+                    i++;
+                }
+            }
+            
+            sort(a.begin()+start,a.begin()+i);
+                    
+            reverse(a.begin()+start, a.begin()+i);
+            
+            return a;
+                    
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector <int> a(n);
+        for(auto &j:a)
+            cin>>j;
+        Solution s;
+        vector <int> b=s.lexicographicallyLargest(a,n);
+        for(auto i:b)
+            cout<<i<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
